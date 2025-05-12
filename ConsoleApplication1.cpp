bb909506f20b61d668a5831842af430adea2df11
hannuverma <99170465+hannuverma@users.noreply.github.com>
2025-05-12 22:57:48 +0530

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -12,6 +12,11 @@ int remainder(int x, int y);
 
 int main()
 {
+	std::cout << "just enter a number then an operator then another number then press enter to get the answer \n"
+		<< "eg : 2+2 = 4 then use any operator then another number\n"
+		<< "Recognized operators include: +, -, *, /, !(factorial), r(remainder)\n"
+		<< "press c to start over\n";
+
 	char sign{};      
 	long double num1{};
 	long double num2{};
@@ -25,11 +30,13 @@ int main()
 	{
 
 
-		if (!doneOnce)
+
+
+		if (!doneOnce)//we dont have the previous number as the num1
 		{
 			std::cin >> num1 >> sign; // take number one then sign then number 2
 		}
-		else
+		else // we are using the previous result as the num1
 		{
 			num1 = result;
 			std::cin >> sign;
@@ -50,9 +57,9 @@ int main()
 
 		}
 
-		else if (sign == '+' || sign == '/' || sign == '*' || sign == '-' || sign == '%')
+		else if (sign == '+' || sign == '/' || sign == '*' || sign == '-' || sign == 'r') // this doesn't feel right but helps me know what operators i have assigned
 		{
-			std::cin >> num2;
+			std::cin >> num2; // take the num2 here because we dont want num2 for factorial
 
 			doneOnce = true;
 
@@ -73,22 +80,21 @@ int main()
 				result = subtraction(num1, num2);
 			}
 
-			else if (sign == '%')
+			else if (sign == 'r')
 			{
-				result = remainder(static_cast<int>(num1), static_cast<int>(num2));
+				result = remainder(static_cast<int>(num1), static_cast<int>(num2)); //for remainder we need only integer 
 
 				doneOnce = false;
-				std::cout << '\n';
+				std::cout << '\n'; 
 			}
-
 
 		}
 
-		else
+		else 
 		{
 			std::cout << "Unkown operator: " << sign << '\n';
 			std::cin.clear();
-			std::cin.ignore(10000, '\n');
+			std::cin.ignore(10000, '\n'); 
 
 			doneOnce = false;
 		}
